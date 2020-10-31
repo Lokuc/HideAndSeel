1e8ec72c67ee4c4cfb732ac7ea1d75c61b546e0b
Кирилл Удонов <udonov.kirya.21@gmail.com>
2020-10-31 19:11:14 +0200

    синхронизировано движение персонажей и колизия

--- Assets/Scripts/MainGame/Network/Netword.cs
+++ Assets/Scripts/MainGame/Network/Netword.cs
@@ -1,4 +1,5 @@
-﻿using com.severgames.lib.Socket;
+﻿using com.severgames.lib.network;
+using com.severgames.lib.Socket;
 using UnityEngine;
 
 public class Netword : MonoBehaviour
@@ -10,9 +11,9 @@ public class Netword : MonoBehaviour
     private Vector2 vector;
     private Rigidbody2D rigidbody;
     private int count = 0;
-    private int counts = 0;
     private bool move = false;
     private Vector2 temp;
+    private VectorPull vectors;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class Netword : MonoBehaviour
         rigidbody = client1.GetComponent<Rigidbody2D>();
         vector = new Vector2();
         temp = new Vector2(0,0);
+        vectors = new VectorPull();
         Client.getClient().setNetwork(this);
     }
 
@@ -31,15 +33,7 @@ public class Netword : MonoBehaviour
 
     public void Move(string name, float x, float y)
     {
-        Debug.Log(x + " F " + y);
-
-        vector.x = x;
-        vector.y = y;
-        Debug.Log(vector.x + " H " + vector.y);
-        count++;
-        Debug.Log("CO" + count);
-        move = false;
-
+        vectors.add(x, y);
     }
 
     private void FixedUpdate()
@@ -48,11 +42,7 @@ public class Netword : MonoBehaviour
         temp.x = 0;
         temp.y = 0;
         rigidbody.velocity = temp;
-        rigidbody.AddForce(vector, ForceMode2D.Force);
-        vector.x = 0;
-        vector.y = 0;
-        counts++;
-        Debug.Log("SE" + counts);
+        rigidbody.AddForce(vectors.getLast(), ForceMode2D.Force);
     }
 
 }
---
+++ Assets/Scripts/MainGame/Network/VectorPull.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace com.severgames.lib.network
+{
+    public class VectorPull
+    {
+        private Vector2[] vectors;
+        private int count;
+        private String poradok;
+        private String temp;
+        private int num;
+
+        public void add(float x, float y)
+        {
+            add(new Vector2(x, y));
+        }
+        public VectorPull()
+        {
+            vectors = new Vector2[10];
+            for(int i=0;i<10;i++){
+                vectors[i] = new Vector2(0,0);
+            }
+            count = 0;
+            poradok = "";
+        }
+
+        public void add(Vector2 vec)
+        {
+            vectors[count] = vec;
+            poradok += count;
+            count++;
+            if (count >= 10)
+            {
+                count = 0;
+            }        
+        }
+
+        public Vector2 getLast()
+        {
+            if (poradok.Equals(""))
+            {
+                return new Vector2(0, 0);
+            }
+            return vectors[getNum()];
+        }
+
+        private int getNum()
+        {
+            temp = poradok.Substring(1, poradok.Length - 1);
+            num = Convert.ToInt32(poradok.Substring(0,1));
+            poradok = temp;
+            return num;
+        }
+        
+        
+
+    }
+
+}
+
